d0544dd324fe893de922d2ec34dabab65612725b
Jeroen Engels <jfm.engels@gmail.com>
2016-07-09 17:26:58 +0200

    Add injection functionality

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 'use strict';
-module.exports = function (str, opts) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
-	}
 
-	opts = opts || {};
+function injectSingle(tag, newContent, previousContent) {
+  var tagToLookFor = '<!-- ' + tag + ':';
+  var closingTag = '-->';
+  var startOfOpeningTagIndex = previousContent.indexOf(tagToLookFor + 'START');
+  var endOfOpeningTagIndex = previousContent.indexOf(closingTag, startOfOpeningTagIndex);
+  var startOfClosingTagIndex = previousContent.indexOf(tagToLookFor + 'END', endOfOpeningTagIndex);
+  if (startOfOpeningTagIndex === -1 || endOfOpeningTagIndex === -1 || startOfClosingTagIndex === -1) {
+    return previousContent;
+  }
+  return previousContent.slice(0, endOfOpeningTagIndex + closingTag.length) +
+    newContent +
+    previousContent.slice(startOfClosingTagIndex);
+}
 
-	return str + ' & ' + (opts.postfix || 'rainbows');
+module.exports = function inject(newContent, previousContent) {
+  return Object.keys(newContent)
+    .reduce(function (res, tag) {
+      return injectSingle(tag, newContent[tag], res);
+    }, previousContent);
 };
--- test.js
+++ test.js
@@ -1,6 +1,102 @@
 import test from 'ava';
-import fn from './';
+import inject from './';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+test('should replace the content inside a tag', t => {
+  const newContent = {
+    'MY-AWESOME-TAG': 'I want to break free'
+  };
+  const previousContent = [
+    '# project',
+    '',
+    '<!-- MY-AWESOME-TAG:START -->FOO BAR BAZ<!-- MY-AWESOME-TAG:END -->',
+    '',
+    'End'
+  ].join('\n');
+  const expected = [
+    '# project',
+    '',
+    '<!-- MY-AWESOME-TAG:START -->I want to break free<!-- MY-AWESOME-TAG:END -->',
+    '',
+    'End'
+  ].join('\n');
+
+  const result = inject(newContent, previousContent);
+
+  t.true(result === expected);
+});
+
+test('should not replace anything if tag is not found', t => {
+  const newContent = {
+    'MY-AWESOME-TAG': 'I want to break free'
+  };
+  const previousContent = [
+    '# project',
+    '',
+    'End'
+  ].join('\n');
+
+  const result = inject(newContent, previousContent);
+
+  t.true(result === previousContent);
+});
+
+test('should replace multiple tags', t => {
+  const newContent = {
+    'MY-TAG-1': 'this is tag 1',
+    'MY-TAG-2': 'this is tag 2',
+    'I-AM-NOT-THERE': 'I should not appear'
+  };
+  const previousContent = [
+    '# project',
+    '',
+    '<!-- MY-TAG-1:START -->FOO BAR BAZ<!-- MY-TAG-1:END -->',
+    '',
+    '<!-- MY-TAG-2:START -->FOO BAR BAZ<!-- MY-TAG-2:END -->',
+    '',
+    'End'
+  ].join('\n');
+  const expected = [
+    '# project',
+    '',
+    '<!-- MY-TAG-1:START -->this is tag 1<!-- MY-TAG-1:END -->',
+    '',
+    '<!-- MY-TAG-2:START -->this is tag 2<!-- MY-TAG-2:END -->',
+    '',
+    'End'
+  ].join('\n');
+
+  const result = inject(newContent, previousContent);
+
+  t.true(result === expected);
+});
+
+test('should be able to inject multiline content', t => {
+  const newContent = {
+    'MY-TAG-1': '\n\nI\nAm\nOn multiple\nlines\n\n'
+  };
+  const previousContent = [
+    '# project',
+    '',
+    '<!-- MY-TAG-1:START -->FOO BAR BAZ<!-- MY-TAG-1:END -->',
+    '',
+    'End'
+  ].join('\n');
+  const expected = [
+    '# project',
+    '',
+    '<!-- MY-TAG-1:START -->',
+    '',
+    'I',
+    'Am',
+    'On multiple',
+    'lines',
+    '',
+    '<!-- MY-TAG-1:END -->',
+    '',
+    'End'
+  ].join('\n');
+
+  const result = inject(newContent, previousContent);
+
+  t.true(result === expected);
 });
